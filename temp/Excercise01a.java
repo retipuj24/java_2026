8d03a82f53b042a7119211444478b57d483e43d8
retipuj24 <98254644+retipuj24@users.noreply.github.com>
2026-03-28 04:53:25 +0000

    for updating at home

---
+++ temp/Excercise01a.java
@@ -0,0 +1,27 @@
+package temp;
+
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Predicate;
+
+public class Excercise01a {
+ 
+
+    public static void main(String[] args) throws java.io.IOException {
+        System.out.print("Enter three numbers: ");
+        Scanner input = new Scanner(System.in);
+    
+        int num1 = input.nextInt();
+        int num2 = input.nextInt();
+        int num3 = input.nextInt();
+
+        List<Integer> numbers = List.of(num1, num2, num3);
+        // numbers.stream()
+        //         .sorted()
+        //         .forEach(System.out::println); 
+        int largest = numbers.stream()
+                .max(Integer::compare)
+                .orElseThrow(() -> new IllegalArgumentException("No numbers provided"));
+        System.out.println("The largest number is: " + largest);
+    }   
+}
