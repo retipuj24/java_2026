6237c2dfa7576a0bcec15298eeba35d34063fab4
retipuj24 <98254644+retipuj24@users.noreply.github.com>
2026-03-28 03:40:34 +0000

    exercise 01 - Mar 28 2026

---
+++ temp/Exercise01.java
@@ -0,0 +1,24 @@
+package temp;
+
+import java.util.Scanner;
+
+public class Exercise01 {
+    public static void main(String[] args) throws java.io.IOException {
+        System.out.print("Enter three numbers: ");
+        Scanner input = new Scanner(System.in);
+        
+        int num1 = input.nextInt();
+        int num2 = input.nextInt();
+        int num3 = input.nextInt();
+
+        if(num1 > num2 && num1 > num3) {
+            System.out.println("The largest number is: " + num1);
+        } else if(num2> num1 && num2 > num3) {
+            System.out.println("The largest number is: " + num2);
+        } else if(num1 == num2 && num1 == num3 && num2 == num3) {
+            System.out.println("All numbers are equal: " + num1);
+        } else {
+            System.out.println("The largest number is: " + num3);
+        }
+    }
+}
