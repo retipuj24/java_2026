49da9fada7a417861b7beabb554723dff7f0036b
retipuj24 <retipuj24@gmail.com>
2026-04-02 13:14:07 +0800

    new updated exercise

--- exercises/NumberToDayOfWeek.java
+++ exercises/NumberToDayOfWeek.java
@@ -17,35 +17,45 @@ public class NumberToDayOfWeek {
             System.out.println("Do you want to exit this program? (y/n) > ");
             input = scanner.next().toLowerCase();
         } while (!input.equalsIgnoreCase("y"));
+        scanner.close();
     }
 
     public static void getInput(int inputNum){
         int inputNumber = inputNum ;
-
-                
+        String msg = "=============================================================";
+        // String msg2 = "\"You have entered \" +inputNumber + \" equivalent as \" + dayEquivalentInWeek[--inputNumber] + \" of the Week.\\n\"";
+        
         switch (inputNumber) {
-
+            
             case 1:
-                System.out.println("\nYou have entered " +inputNumber + " equivalent as " + dayEquivalentInWeek[--inputNumber] + " of the Week.\n");
+                System.out.println(msg);
+                System.out.println("You have entered " +inputNumber + " equivalent as " + dayEquivalentInWeek[--inputNumber] + " of the Week.");
+                System.out.println(msg);
                 break;
             case 2:
-                System.out.println("\nYou have entered " +inputNumber + " equivalent as " + dayEquivalentInWeek[--inputNumber] + " of the Week.\n");               
-                break;
+                System.out.println(msg);
+                System.out.println("You have entered " +inputNumber + " equivalent as " + dayEquivalentInWeek[--inputNumber] + " of the Week.");
+                System.out.println(msg);                break;
             case 3:
-                System.out.println("\nYou have entered " +inputNumber + " equivalent as " + dayEquivalentInWeek[--inputNumber] + " of the Week.\n");               
-                break;
+                System.out.println(msg);
+                System.out.println("You have entered " +inputNumber + " equivalent as " + dayEquivalentInWeek[--inputNumber] + " of the Week.");
+                System.out.println(msg);                break;
             case 4:
-                System.out.println("\nYou have entered " +inputNumber + " equivalent as " + dayEquivalentInWeek[--inputNumber] + " of the Week.\n");               
-                break;
+                System.out.println(msg);
+                System.out.println("You have entered " +inputNumber + " equivalent as " + dayEquivalentInWeek[--inputNumber] + " of the Week.");
+                System.out.println(msg);                break;
             case 5:
-                System.out.println("\nYou have entered " +inputNumber + " equivalent as " + dayEquivalentInWeek[--inputNumber] + " of the Week.\n");               
-                break;
+                System.out.println(msg);
+                System.out.println("You have entered " +inputNumber + " equivalent as " + dayEquivalentInWeek[--inputNumber] + " of the Week.");
+                System.out.println(msg);                break;
             case 6:
-                System.out.println("\nYou have entered " +inputNumber + " equivalent as " + dayEquivalentInWeek[--inputNumber] + " of the Week.\n");               
-                break;
+                System.out.println(msg);
+                System.out.println("You have entered " +inputNumber + " equivalent as " + dayEquivalentInWeek[--inputNumber] + " of the Week.");
+                System.out.println(msg);                break;
             case 7:
-                System.out.println("\nYou have entered " +inputNumber + " equivalent as " + dayEquivalentInWeek[--inputNumber] + " of the Week.\n");               
-                break;
+                System.out.println(msg);
+                System.out.println("You have entered " +inputNumber + " equivalent as " + dayEquivalentInWeek[--inputNumber] + " of the Week.");
+                System.out.println(msg);                break;
             default:
                 System.out.println("\nSorry, Input must be from 1 to 7 only.\n");
                 break;
