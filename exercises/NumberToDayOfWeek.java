4b287c848ae7172163ab67d055f96736c9798fb3
retipuj24 <retipuj24@gmail.com>
2026-04-02 12:39:55 +0800

    new exercises in progress

---
+++ exercises/NumberToDayOfWeek.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class NumberToDayOfWeek {
+    static String[] dayEquivalentInWeek = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"} ;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = "y";
+        do {
+            System.out.print("\nPlease enter 1 to 7 only for day in Week equivalent: > ");
+            if (scanner.hasNextInt()) {
+                int inputNumber = scanner.nextInt();
+                getInput(inputNumber);
+            } else {
+                System.out.println("THAT'S NOT A VALID NUMBER!");
+                scanner.next();   
+            }
+        } while (!input.equalsIgnoreCase("y"));
+    }
+
+    public static void getInput(int inputNum){
+        int inputNumber = inputNum ;
+        
+        switch (inputNumber) {
+            case 1:
+                System.out.println("\nYou have entered " +inputNumber + " equivalent as " + dayEquivalentInWeek[--inputNumber] + " of the Week.\n");
+                break;
+            case 2:
+                System.out.println("\nYou have entered " +inputNumber + " equivalent as " + dayEquivalentInWeek[--inputNumber] + " of the Week.\n");               
+                break;
+            case 3:
+                System.out.println("\nYou have entered " +inputNumber + " equivalent as " + dayEquivalentInWeek[--inputNumber] + " of the Week.\n");               
+                break;
+            case 4:
+                System.out.println("\nYou have entered " +inputNumber + " equivalent as " + dayEquivalentInWeek[--inputNumber] + " of the Week.\n");               
+                break;
+            case 5:
+                System.out.println("\nYou have entered " +inputNumber + " equivalent as " + dayEquivalentInWeek[--inputNumber] + " of the Week.\n");               
+                break;
+            case 6:
+                System.out.println("\nYou have entered " +inputNumber + " equivalent as " + dayEquivalentInWeek[--inputNumber] + " of the Week.\n");               
+                break;
+            case 7:
+                System.out.println("\nYou have entered " +inputNumber + " equivalent as " + dayEquivalentInWeek[--inputNumber] + " of the Week.\n");               
+                break;
+            default:
+                System.out.println("\nSorry, Input must be from 1 to 7 only.\n");
+                break;
+        }
+    }
+}
