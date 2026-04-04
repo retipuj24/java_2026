4391dcf2909283ba41c98688af60384d284bfe6c
retipuj24 <retipuj24@gmail.com>
2026-04-04 12:34:51 +0800

    modified to make to print success

--- exercises/Exercise01.java
+++ exercises/Exercise01.java
@@ -15,7 +15,7 @@ public class Exercise01 {
             System.out.println("The largest number is: " + num1);
         } else if(num2> num1 && num2 > num3) {
             System.out.println("The largest number is: " + num2);
-        } else if(num1 == num2 && num1 == num3 && num2 == num3) {
+        } else if(num1 == num2 && num2 == num3) {
             System.out.println("All numbers are equal: " + num1);
         } else {
             System.out.println("The largest number is: " + num3);
