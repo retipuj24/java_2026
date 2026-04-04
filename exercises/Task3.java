f5a9ec34bfb394629e998801b97998544f2db29e
retipuj24 <retipuj24@gmail.com>
2026-04-04 12:37:23 +0800

    modified to make to print success eand deleted unnecessary code

---
+++ exercises/Task3.java
@@ -0,0 +1,41 @@
+/** 
+* Class name Task3:
+* Change the variables in the first section, 
+* so that each if statement resolves as true.
+
+        String a = new String("Wow");
+        String b = "Wow";
+        String c = a;
+        String d = c;
+
+        boolean b1 = a == b;
+        boolean b2 = d.equals(b + "!");
+        boolean b3 = !c.equals(a);
+
+        if (b1 && b2 && b3) {
+            System.out.println("Success!");
+        }
+*/
+
+
+public class Task3 {
+    public static void main(String[] args) {
+        // String a = new String("Wow");
+        String a = "Wow"; // change to directly assign the string literal to a, so it will be interned and point to the same memory location as b
+        String b = "Wow";
+        String c = a;
+        // add code here
+        c = a+"!";
+        String d = c;        
+       
+
+        boolean b1 = a == b;
+        boolean b2 = d.equals(b + "!");
+        boolean b3 = !c.equals(a);
+
+        if (b1 && b2 && b3) {
+            System.out.println("Success!");
+        }
+        // System.out.println("nothing to see here...");
+    }
+}
