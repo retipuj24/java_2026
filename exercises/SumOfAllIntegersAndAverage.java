84d49347fc2742c47cce5d29e8cb4a943f94dda5
retipuj24 <retipuj24@gmail.com>
2026-04-05 22:26:50 +0800

    updated

--- exercises/SumOfAllIntegersAndAverage.java
+++ exercises/SumOfAllIntegersAndAverage.java
@@ -20,7 +20,7 @@ public static void main(String[] args) {
                 System.out.println("THAT'S NOT A VALID NUMBER!");
                 scanner.next();   
             }
-            System.out.print("\nPress any key to continue, Press 'y' if you want to exit this program. Exit?(y) > ");
+            System.out.print("\nPress any key to continue, \nPress 'y' if you want to exit this program. Exit?(y) > ");
             input = scanner.next().toLowerCase();
             clearScreen();
             
@@ -42,10 +42,16 @@ public static void main(String[] args) {
         //String hLine = "--------------------------------------------------------------";
         String yellowBG = "\u001B[43m";
         String blackText = "\u001B[30m";
+        String yellowText ="\u001B[33m";
         String reset = "\u001B[0m";
+
+        int sum = numbers.stream().mapToInt(Integer::intValue).sum();
+        double average = numbers.size() == 0? 0:sum/numbers.size();
+        
         System.out.println(msg);
         System.out.println("Entered integers are: " + yellowBG + blackText + numbers + reset);
-        System.out.println("Sum of all entered integers is: " + yellowBG + blackText + numbers.stream().mapToInt(Integer::intValue).sum() + reset);
+        System.out.println(yellowText + "SUM "+ reset +"of all entered integers is: " + yellowBG + blackText + sum + reset);
+        System.out.println("The " + yellowText + "AVERAGE "+ reset + "is: " + yellowBG + blackText + average + reset);
         System.out.println(msg);
     }
 }
