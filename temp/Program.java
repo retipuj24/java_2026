5d442440c46730dee78be81e9631628f143de835
retipuj24 <retipuj24@gmail.com>
2026-04-14 10:21:20 +0800

    AAA

---
+++ temp/Program.java
@@ -0,0 +1,90 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Program{
+
+
+    // public final static void main(String[] a){
+        // int[]a = new int[x];
+        // Duplicate local variable a
+        // at Program.main(Program.java:4)
+        // a[2] = 2;
+    
+        // for (int i:a)
+        //     System.out.print(i);
+        // ============================================
+        
+    // }
+
+    //    static int x = 2;
+    // public static void main(String[] args) {
+    //     int[] a = {3,1,22,5,11,2};
+    //     System.out.println(" before sort: > " + a[1]);
+	// 		sort(a);
+    //     for (int i : a) {
+    //         System.out.println(i);
+    //     }
+    //     System.out.println("after sort: > " + a[1]);
+    // }
+    // answer here is the < import static java.util.Arrays.*; >
+    // ==================================================
+    // static int x = 20;
+    // public static void main(String args[]){
+	// 	Program pr = new Program();
+	// 		pr.x = 5;
+	// 	int y = x/pr.x;
+	// 		System.out.print("y = ");
+	//          		System.out.print(y);
+    
+    //     char c=65;
+    //     System.out.println("===== " + c);
+
+    //     String str = " a goof of a swan     ";
+    //     System.out.println("s>" + str +"<e");
+    //     str.trim();
+    //     System.out.println("s>" + str.toUpperCase() +"<e");
+    //     System.out.println("s>" + str +"<e");
+    //     StringBuilder sb = new StringBuilder();
+
+    //     String otherStr = " a goof of a swan     ";
+    //     String obiStr = new String(" a goof of a swan     ");
+    //     System.out.println(str);
+    //     System.out.println(otherStr);
+    //     System.out.println(str == otherStr);
+    //     System.out.println(str == obiStr);
+    //     System.out.println(str.equals(obiStr));
+    //     System.out.println("obiStr: > " + obiStr);
+
+    //     float fn1 = 1f;
+    //     System.out.println(fn1);
+         
+    // }
+    // =======================================================
+    // public static void main(String[] args) { 
+    //     List<String> list = new ArrayList<String>(); 
+    //     list.add("B"); list.add("C"); list.add("A"); 
+    // 	System.out.print(" " + list.indexOf("D")); 
+	// 		Collections.sort(list); 
+	//     	System.out.println(" " + list.indexOf("A")); 
+    // } 
+    // =======================================================
+    // int i = j;
+    // static int j = 1;
+    // public static void main(String args[]){
+    //     System.out.print(new Program().i + " ");
+    //     System.out.print(j);
+    // }
+
+    // static{	j = 2;	}
+    // =======================================================
+    public static void main(String args[]){
+        // int x = 011;
+
+        // int y  = 0b2;
+        // System.out.print(x+y);
+        System.out.println(011);
+    }
+// ======================================================
+
+}
