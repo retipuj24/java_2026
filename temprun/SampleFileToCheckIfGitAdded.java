6850257fb86440ccaf0e15918b8f318d36628b70
retipuj24 <retipuj24@gmail.com>
2026-04-17 10:45:51 +0800

    added SampleFileToCheckIfGitAdded

---
+++ temprun/SampleFileToCheckIfGitAdded.java
@@ -0,0 +1,7 @@
+public class SampleFileToCheckIfGitAdded {
+    public static void main(String[] args) {
+        String className = java.lang.invoke.MethodHandles.lookup().lookupClass().getSimpleName();
+        System.out.println("className: " + className);
+    }
+
+}
