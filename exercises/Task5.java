19e8fc376f314268c52980064e669b576e17546d
retipuj24 <retipuj24@gmail.com>
2026-04-24 10:57:26 +0800

    update Task5

--- exercises/Task5.java
+++ exercises/Task5.java
@@ -1,79 +1,81 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 public class Task5 {
     public static String input = "";
     public static String message = "";
+    List<String> enteredList;
+    List<Number> numberList;
+
+    public Task5(){
+        enteredList = new ArrayList<>();
+        numberList = new ArrayList<>();
+    }
     
     public static void main(String[] args) {
-  
+        Task5 task = new Task5();
+        boolean toContinue = true;
         do{
-            getInput("Enter three(3) numbers: > " );
-            showProcessedString(input);
-        }while(true);
+            // clearScreen();
+            // task.printTheList();      
+            task.getInput("Enter three(3) numbers: > " );
+            task.processTheInput(input);
+            task.getInput("Enter three(3) numbers: > " );
+            task.processTheInput(input);
+            clearScreen();
+            task.printTheList();   
+            toContinue = task.countEqualsTo(3);             
+        }while(toContinue);
     } 
 
-    public static void showProcString(String str){
-
-        List<String> enteredStrings = new ArrayList<>();
-        List<Number> numberList = new ArrayList<>();
-        double n1 = 0;
         
-        try {
-            if(numberList.size() < 3){
-                n1 = Double.parseDouble(str);
-                numberList.add(n1);
-            }else{
-                enteredStrings.add(str);
-            }
-        } catch (NumberFormatException e) { 
-            // System.out.println("Its not a number");
-            message = "Its not a number";
-            enteredStrings.add(str);
+    public void processTheInput(String input){
+        Pattern p = Pattern.compile("^[+-]?\\d*(\\.\\d+)?$");
+        Matcher matcher = p.matcher(input);
+        boolean isNumeric = matcher.find();  
+        
+        if(isNumeric){
+            try { this.numberList.add(Integer.parseInt(input)); } 
+            catch (NumberFormatException e) { this.numberList.add(Double.parseDouble(input)); }
         }
-        
-    }
- 
-    public static void showProcessedString(String str){
-        
+    }   
+
+    public void printTheList(){
         String hLine = "---------------------------------------------------------------------------------------------";
         String yellowBG = "\u001B[43m";
         String blackText = "\u001B[30m";
         String boldRedText = "\033[1;31m";
         String boldYellowText ="\033[1;33m";
+        String greeenText = "\u001B[32m";
         String reset = "\u001B[0m";
-        String vowels = "AEIOUaeiou";
-        String consonants = "CDFGHJKLMNPQRSTVWXYZcdfghjklmnpqrstvwxyz";
-        String numbers = "01234567890.";
-        List<Character> selectedChars = new ArrayList<>();
-        List<Number> numberList = new ArrayList<>();
+        System.out.println(hLine);
+        System.out.println("Entered numbers: " + yellowBG + blackText + this.numberList + reset);
         
-        System.out.println(hLine);
-        System.out.printf(boldYellowText +"Entered string: " + reset);
-        System.out.print(blackText +yellowBG + str+ reset +"\n");
-        System.out.println(hLine);
+        List<Number> arr = new ArrayList<>(this.numberList);
+        Collections.sort(arr, Comparator.comparingDouble(Number::doubleValue));
+        // System.out.println(arr);
         
-        System.out.print(boldYellowText + "VOWELS"+ reset +" in entered string: ");    
-        selectedChars = str.chars().mapToObj(c -> (char)c)
-                                .filter(c-> vowels.indexOf(c)!= -1)
-                                .collect(Collectors.toList());
-        System.out.printf(blackText + yellowBG + selectedChars + reset + "\n");   
-        System.out.print(boldYellowText + "CONSONANTS"+ reset +" in entered strings: ");   
-        selectedChars.clear();
-        selectedChars = str.chars().mapToObj(c -> (char)c)
-                                .filter(c-> consonants.indexOf(c) != -1)
-                                .collect(Collectors.toList());
-        System.out.printf(blackText + yellowBG + selectedChars + reset + "\n");   
+        boolean allSame = new HashSet<>(numberList).size() == 1;
+        String comparedString = allSame?"THE SAME." : "NOT THE SAME.";
+
+        System.out.println("ALL Numbers are " + greeenText + comparedString +  reset);
+        System.out.println(greeenText + "HIGHEST NUMBER : "+ reset + yellowBG + blackText + arr.get(arr.size()-1)+reset);
         System.out.println(hLine);
-    }    
-    public static void clearScreen() {
-        System.out.print("\033[H\033[2J");
-        System.out.flush();
     }
-    
-    static public void getInput(String msg){
+       
+    public boolean countEqualsTo(int size){
+        return this.numberList.size() < size;
+    }
+
+    public void getInput(String msg){
         String hLine = "---------------------------------------------------------------------------------------------";
         String yellowBG = "\u001B[43m";
         String blackText = "\u001B[30m";
@@ -88,15 +90,15 @@ public class Task5 {
         String answer = scanner.nextLine();
         if(answer.equalsIgnoreCase("y")){
             System.exit(0);
-        }else{
-            
-            // checked this out - its possible to call the class static variable by instance variable
-            // CheckVowelsConsonants cvc = new CheckVowelsConsonants();
-            // cvc.input = answer;
-            
-            CheckVowelsConsonants.input = answer;
-            clearScreen();
-        }   
-    }   
+        }
+
+        else{    
+            Task5.input = answer;
+        }  
+    }  
     
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
 }
